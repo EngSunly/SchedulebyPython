a1247b9df67b744ff096e698273ac7c2b52573ef
USER <USER@SUNLYGF63>
2023-06-13 12:29:15 +0700

    Add project files.

---
+++ PythonApplication1.py
@@ -0,0 +1,46 @@
+
+from datetime import datetime
+date = datetime.now().date()
+dayweek = date.weekday()+1
+day = dayweek
+    
+monday = 'Info Sys Analysis and Computer Network'
+tuesday = 'Info Sys Analysis and Java Program'
+wednesday = 'Statistic Analysis and C# Dot Net Program'
+thursday = 'Computer Network and Java Program'
+friday = 'Statistic Analysis and C# Dot Net Program'
+saturday = 'Enjoy'
+sunday = 'Enjoy'
+
+
+def sec():
+    print('A simple program for finding schedule of the day\n')
+    print('Today is Day '+str(dayweek)+' of the week')
+
+    if day == 1:
+        print('Monday')
+        print(monday+'\nTmr\n'+tuesday)
+    if day == 2:
+        print('Tuesday')
+        print(tuesday+'\nTmr\n'+wednesday)
+    if day == 3:
+        print('Wednesday ')
+        print(wednesday+'\nTmr\n'+thursday)
+    if day == 4:
+        print('Thursday')
+        print(thursday+'\nTmr\n'+friday)
+    if day == 5:
+        print('Friday')
+        print(friday+'\nTmr\n'+saturday)
+    if day == 6:
+        print('Saturday')
+        print(saturday+'\nTmr\n'+sunday)
+    if day == 7:
+        print('Sunday')
+        print(sunday+'\nTmr\n'+monday)
+    print("\nFull Week Class\n"+monday+"\t"+tuesday+"\t"+wednesday+"\n"+thursday+"\t"+friday+"\t"+saturday)
+    input('Press Any key to Exit ')
+
+
+sec()
+
